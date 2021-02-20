d55804f7bc8d7bbc8fec857907dbac666f07034c
AURYA <info@aurya.de>
2021-02-20 21:57:28 +0000

    Changed to references

--- main.cpp
+++ main.cpp
@@ -28,57 +28,47 @@ struct T
 
 struct Struct1                                //4
 {
-    T* compare(T* a, T* b) //5
+    const T& compare(const T& a, const T& b) //5
     {
-        if(a != nullptr && b != nullptr) 
-        {
-            if( a->value < b->value ) return a;
-            if( a->value > b->value ) return b;
-        }
-        return nullptr;
+        if( a.value < b.value ) return a;
+        if( a.value > b.value ) return b;
+        return a;   // Added to remove the compiler warnings "control may reach end of non-void function [-Wreturn-type]"
     }
 };
 
 struct U
 {
     float name1 { 30 }, name2 { 20 };
-    float memberFunction(float* updatedValue)      //12
+    float memberFunction(const float& updatedValue)      //12
     {
-        if(updatedValue != nullptr) 
+
+        std::cout << "U's name1 value: " << name1 << std::endl;
+        name1 = updatedValue;
+        std::cout << "U's name1 updated value: " << name1 << std::endl;
+        while( std::abs(name2 - name1) > 0.001f )
         {
-            std::cout << "U's name1 value: " << name1 << std::endl;
-            name1 = *updatedValue;
-            std::cout << "U's name1 updated value: " << name1 << std::endl;
-            while( std::abs(name2 - name1) > 0.001f )
-            {
-                name1 -= .5f;
-                name2 -= 1.0f;
-            }
-            std::cout << "U's name2 updated value: " << name2 << std::endl;
-            return name2 * name1;   
+            name1 -= .5f;
+            name2 -= 1.0f;
         }
-        return 0;
+        std::cout << "U's name2 updated value: " << name2 << std::endl;
+        return name2 * name1;   
     }
 };
 
 struct Struct2
 {
-    static float staticFunctionA(U* that, float* updatedValue )        //10
+    static float staticFunctionA(U& that, const float& updatedValue )        //10
     {
-        if(that != nullptr && updatedValue != nullptr) 
+        std::cout << "U's name1 value: " << that.name1 << std::endl;
+        that.name1 = updatedValue;
+        std::cout << "U's name1 updated value: " << that.name1 << std::endl;
+        while( std::abs(that.name2 - that.name1) > 0.001f )
         {
-            std::cout << "U's name1 value: " << that->name1 << std::endl;
-            that->name1 = *updatedValue;
-            std::cout << "U's name1 updated value: " << that->name1 << std::endl;
-            while( std::abs(that->name2 - that->name1) > 0.001f )
-            {
-                that->name1 -= .5f;
-                that->name2 -= 1.0f;
-            }
-            std::cout << "U's name2 updated value: " << that->name2 << std::endl;
-            return that->name2 * that->name1;
+            that.name1 -= .5f;
+            that.name2 -= 1.0f;
         }
-        return 0;
+        std::cout << "U's name2 updated value: " << that.name2 << std::endl;
+        return that.name2 * that.name1;
     }
 };
         
@@ -102,16 +92,15 @@ int main()
     T t2(20 , "b");                                             //6
     
     Struct1 f;                                            //7
-    auto* smaller = f.compare(&t1 , &t2); 
-    if(smaller != nullptr)                             //8
-        std::cout << "the smaller one is << " << smaller->name << std::endl; //9 
+    auto& smaller = f.compare(t1, t2); 
+    std::cout << "the smaller one is << " << smaller.name << std::endl; //9 
     
     U u3;
     float updatedValue = 5.f;
-    std::cout << "[static func] u3's multiplied values: " << Struct2::staticFunctionA(&u3 , &updatedValue) << std::endl;                  //11
+    std::cout << "[static func] u3's multiplied values: " << Struct2::staticFunctionA(u3 , updatedValue) << std::endl;                  //11
     
     U u4;
-    std::cout << "[member func] u4's multiplied values: " << u4.memberFunction( &updatedValue ) << std::endl;
+    std::cout << "[member func] u4's multiplied values: " << u4.memberFunction( updatedValue ) << std::endl;
 
     std::cout << "- complete -" << std::endl;
     
